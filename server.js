6de1fb633afbc3f3b5cf9951a4a29b64f9b17e2b
Nishant Thakur <159132420+NishantT-dev@users.noreply.github.com>
2026-04-21 18:59:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+// importing modules and packages
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import mainRouter from "./app.js";
+
+// configuring dotenv for env variables
 dotenv.config();
-const app=express();
 mongoose.connect(process.env.MONGO_URI)
+
+const app=express();
+// app level middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(mainRouter);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
